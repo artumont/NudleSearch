f7233e9ee53c2811d734476173c311d2830352b2
artumont <197291181+artumont@users.noreply.github.com>
2025-05-11 01:07:38 -0600

    refactor: add type hints and implement header retrieval
    This commit introduces type hints for `custom_user_agent` and `proxy_checks` in `ConnectionManager` for better code clarity. It also implements the `_get_headers` method to provide default and custom User-Agent headers.

--- crawler/nudlecrawler/connection/__init__.py
+++ crawler/nudlecrawler/connection/__init__.py
@@ -17,8 +17,8 @@ class ConnectionManager:
         self.timeout: int = int(os.getenv("TIMEOUT", 10))
 
         # @param: Non-argument variables
-        self.custom_user_agent = None
-        self.proxy_checks = [
+        self.custom_user_agent: str = None
+        self.proxy_checks: List[ProxyChecks] = [
             ProxyChecks.ALIVE,
             ProxyChecks.WORKING,
             ProxyChecks.CLOUDFLARE,
@@ -34,7 +34,7 @@ class ConnectionManager:
         self.custom_user_agent = user_agent
         logger.debug(
             f"User agent set to: {user_agent if user_agent else 'default'}")
-    
+
     def set_proxy_checks(self, checks: List[ProxyChecks]) -> None:
         """Set the proxy checks to perform."""
         if not isinstance(checks, list):
@@ -47,17 +47,24 @@ class ConnectionManager:
     async def post(self, url: str, payload: dict) -> httpx.Response:
         """Make a POST request to the given URL with the given payload."""
         pass
-    
+
     async def get(self, url: str) -> httpx.Response:
         """Make a GET request to the given URL."""
         pass
-    
+
     # @context: Private
     def _get_headers(self) -> dict:
-        pass
-    
+        return {
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            ) if self.custom_user_agent is None else self.custom_user_agent,
+            "Accept-Language": "en-US,en;q=0.5",
+            "Connection": "keep-alive",
+            "Accept": "*/*",
+        }
     async def _get_proxy(self) -> Proxy | Exception:
         pass
-    
+
     async def _check_proxy(self, proxy: Proxy) -> bool:
         pass

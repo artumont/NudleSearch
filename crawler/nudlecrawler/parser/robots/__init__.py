4930fc327e370b4ced073cf6c6d9a06a562f7e22
artumont <197291181+artumont@users.noreply.github.com>
2025-05-16 12:17:11 -0600

    refactor(robots): remove unused RobotsParser class and related imports
    todo: make the parser class

--- crawler/nudlecrawler/parser/robots/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import Set
-from nudlecrawler.parser.robots.models import DomainRules
-
-class RobotsParser:
-    def __init__(self, domain: str):
-        self.domain = domain
-        self.rules: DomainRules = DomainRules()
